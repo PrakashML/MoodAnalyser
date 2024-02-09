27a24d859d6ef274c397504e1e491550cad313a0
PrakashML <prakashkrishnan08@gmail.com>
2024-02-09 15:13:07 +0530

    initial commit

---
+++ MoodAnalyser/src/com/mood/analyser/Main.java
@@ -0,0 +1,8 @@
+package com.mood.analyser;
+
+public class Main {
+    public static void main(String[] args) {
+        MoodAnalyser m1 = new MoodAnalyser();
+        System.out.println("Welcome to MoodAnalyser!!");
+    }
+}
---
+++ MoodAnalyser/src/com/mood/analyser/MoodAnalyser.java
@@ -0,0 +1,6 @@
+package com.mood.analyser;
+
+public class MoodAnalyser {
+    public void analyseMood(String msg){
+    }
+}
